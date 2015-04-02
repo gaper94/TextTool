c0dc7f0d696b10e454e4c10321724c7a4dedf208
GAPer <gaper94@gmail.com>
2015-04-02 22:34:16 +0300

    initial file TextEntity

---
+++ TextTool/TextEntity.java
@@ -0,0 +1,11 @@
+package TextTool;
+
+/**
+ * @brief -Class to summarize text entity used in the text tool
+ * @author GAPer
+ * @version 1.0
+ * @date of last modification 4 April 2015
+ */
+public class TextEntity {
+    
+}
