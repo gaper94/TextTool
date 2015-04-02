426ce30e6872019317b165bf93041fd76ad75d2b
GAPer <gaper94@gmail.com>
2015-04-02 22:30:22 +0300

    test commit

---
+++ TextTool/Configurator.java
@@ -0,0 +1,30 @@
+package TextTool;
+
+
+
+
+import java.util.HashMap;
+
+/**
+ * @brief - Class containing configuration settings and functions for the text tool
+ * @author GAPer
+ * @version 1.0
+ * @date of last modification 4 April 2015
+ */
+public class Configurator {
+    /* Map containing all configured languages in Text Tool  */
+    static HashMap<String, Integer> Languages;
+    
+    //languages confugred 
+    public static int GetNumberOfLanguagesConfigured()
+    {
+        return Languages.size();
+    }
+    //insert new language in the Text Tool
+    public static void AddNewLanguage(String LangDscr)
+    {
+        Languages.put(LangDscr, Languages.size()+ 1 );
+    }
+    
+
+}
